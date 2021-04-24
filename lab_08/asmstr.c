6f6abd02e0521dc60f109ed6c9519d008b4c357f
mmd19u540 <mymidi.student@yandex.ru>
2021-04-24 08:37:03 +0300

    FIX: asmlen without rax (add neg)

--- lab_08/asmstr.c
+++ lab_08/asmstr.c
@@ -6,17 +6,16 @@ size_t asmlen(const char *const string)
 
     __asm__(
         ".intel_syntax noprefix\n"
-        "mov rcx, 0xffffffffffffffff\n"
+        "mov rcx, -1\n"
         "mov rdi, %1\n"
         "mov al, 0\n"
         "repne scasb\n"
-        "mov rax, 0xffffffffffffffff\n"
-        "sub rax, rcx\n"
-        "dec rax\n"
-        "mov %0, rax\n"
+        "neg rcx\n"
+        "sub rcx, 2\n"
+        "mov %0, rcx\n"
         : "=r" (len)
         : "r" (string)
-        : "ecx", "rdi", "al", "eax"
+        : "rcx", "rdi", "al"
     );
 
     return len;
