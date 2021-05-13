dc2b90927cf45e199bc1edfe7e7a6aebf1c723e0
mmd19u540 <mymidi.student@yandex.ru>
2021-05-13 23:09:27 +0300

    ADD: asm float sum

--- lab_09/src/float_operations.c
+++ lab_09/src/float_operations.c
@@ -7,7 +7,8 @@ int getFloatSize(void)
 
 float getFloatCSum(float a, float b)
 {
-    return a + b;
+    float res = a + b;
+    return res;
 }
 
 float getFloatCProd(float a, float b)
@@ -15,25 +16,56 @@ float getFloatCProd(float a, float b)
     return a * b;
 }
 
+float getFloatAsmSum(float a, float b)
+{
+    float res;
+
+    __asm__(
+        "fld %1\n"
+        "fld %2\n"
+        "faddp\n"
+        "fstp %0"
+        : "=m" (res)
+        : "m" (a), "m" (b)
+    );
+
+    return res;
+}
+
+float getFloatAsmProd(float a, float b)
+{
+    return a * b;
+}
+
 double getFloatFuncTime(float (*func)(float, float))
 {
-    float a = 1e5;
-    float b = 11e-7;
+    float a = 2.3e38;
+    float b = 1.1e38;
 
-    clock_t time = clock();
+    clock_t start = clock();
     
     for (size_t i = 0; i < REPEATS; ++i)
         func(a, b);
 
-    time = clock() - time;
+    clock_t end = clock();
 
-    return (double) time / CLOCKS_PER_SEC / REPEATS;
+    return (double) (end - start) / CLOCKS_PER_SEC / REPEATS;
 }
 
 void printFloatCharacteristics()
 {
     puts("FLOAT");
     printf("Size:     %9d bit\n", getFloatSize());
-    printf("C   sum   %9.4g s\n", getFloatFuncTime(getFloatCSum));
+    printf(
+        "C   sum   %9.4g s res = %g\n",
+        getFloatFuncTime(getFloatCSum),
+        getFloatCSum(2.3e38, 1.1e38)
+    );
+    printf(
+        "ASM sum   %9.4g s res = %g\n",
+        getFloatFuncTime(getFloatAsmSum),
+        getFloatAsmSum(2.3e38, 1.1e38)
+    );
     printf("C   prod: %9.4g s\n", getFloatFuncTime(getFloatCProd));
+    printf("ASM prod: %9.4g s\n", getFloatFuncTime(getFloatAsmProd));
 }
